35506f8422c04ad5d97cd98ff13f489910f7b1e0
ScriptorNox <acansler@fingerlakes.edu>
2025-10-08 20:01:23 +0000

    Submitting phishing_email_detector.py

--- phishing_email_detector.py
+++ phishing_email_detector.py
@@ -1,8 +1,8 @@
 # FILE NAME - phishing_email_detector.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: Asia Cansler
+# DATE: October 7, 2025
+# BRIEF DESCRIPTION: A program that analyzes an email subject line to detect phishing attempts based on common red flags.
 
 
 
@@ -14,9 +14,29 @@
 
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
+print()
 
+subject = input('Enter the email subject line: ')
 
+print()
 
+print('SECURITY ASSESSMENT:')
+
+if 'urgent' in subject.lower() or 'immediate action required' in subject.lower():
+    print('HIGH RISK: Possible phishing attempt.')
+
+elif 'win' in subject.lower() or 'free' in subject.lower():
+    print('MEDIUM RISK: Suspicious offer detected.')
+
+elif 'password reset' in subject.lower():
+    print('LOW RISK: Verify legitimacy with sender.')
+
+else:
+    print('No phishing indicators detected.')
+
+print('------------------------')
+
+print(f'Analyzed subject: "{subject}"')
 
 
 
@@ -77,8 +97,8 @@ Analyzed subject: "Did you request a password reset?"
 '''
 
 1. Was using `in` difficult or was it natural?
-
-
+Using 'in' was not difficult at all. Using 'in' to look for more than one thing was.
+There was a LOT of trial and error involved to get it correct.
 
 
 
@@ -97,7 +117,7 @@ Analyzed subject: "Did you request a password reset?"
 Please gauge your utilization of AI on the following spectrum. Place an "X" in front
 of the appropriate response. Only choose one of the following:
 
-[ ] I did not use AI at all for this lab.
+[X ] I did not use AI at all for this lab.
 [ ] I wrote the initial draft of the software but had AI help me make it better.
 [ ] I fed the lab description to AI and had it generate a response but I modified it.
 [ ] AI created the entire program for me.
@@ -111,7 +131,7 @@ may impede your understanding. Please rate how well you understand the concepts 
 
 [ ] I understand very little about this lab.
 [ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
-[ ] I pretty much get it.
+[X ] I pretty much get it.
 [ ] I'm solid. Totally got it.
 
 '''
